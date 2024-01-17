a73f85c18110ae968259f0a373e5468388c655d2
Absar Syed <absar.syed@dcmail.ca>
2024-01-17 18:43:56 -0500

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,80 @@
+"use strict";
+
+// IIFE - Immediately Invoked Functional Expression
+(function (){
+
+    function DisplayHomePage() {
+        console.log("Called DisplayHomePage")
+
+        let AboutUsBtn = document.getElementById("AboutUsBtn");
+
+        AboutUsBtn.addEventListener("click", function () {
+            location.href = "about.html";
+        });
+
+        let MainContent = document.getElementsByTagName("main")[0];
+
+        let MainParagraph = document.createElement("p");
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        MainParagraph.textContent = "This is my first paragraph.";
+
+        MainContent.appendChild(MainParagraph);
+
+        let FirstString = "This is";
+        let SecondString = `${FirstString} the main paragraph.`; //this is js concatenation, string literals
+        MainParagraph.textContent = SecondString;
+        MainContent.appendChild(MainParagraph);
+
+        let DocumentBody = document.body;
+
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">this is my article paragraph.</p>`;
+        Article.setAttribute("class", "container");
+        Article.innerHTML = ArticleParagraph;
+
+        DocumentBody.appendChild(Article);
+    }
+
+    function DisplayProductPage() {
+        console.log("Called DisplayProductPage")
+    }
+    function DisplayServicePage() {
+        console.log("Called DisplayServicePage")
+    }
+
+    function DisplayAboutPage() {
+        console.log("Called DisplayAboutPage")
+    }
+
+    function DisplayContactPage() {
+        console.log("Called DisplayContactPage")
+    }
+
+    function Start(){
+        console.log("App Started...");
+
+        switch(document.title) {
+
+            case "Home":
+                DisplayHomePage();
+                break;
+            case "About Us":
+                DisplayAboutPage();
+                break;
+            case "Contact Us":
+                DisplayContactPage();
+                break;
+            case "Service":
+                DisplayServicePage();
+                break;
+            case "Product":
+                DisplayProductPage();
+                break;
+
+        }
+    }
+
+    window.addEventListener("load", Start);
+
+})()
